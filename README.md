b004010b4fd178fe50d7b5aee54c882d774acdfc
Daniel Pawlik <pawlik.dan@gmail.com>
2021-03-10 08:58:06 +0100

    Added option for not configure vim from my source
    Install vim script has been changed for someone that want to install
    other vim distribution.

--- README.md
+++ README.md
@@ -8,139 +8,16 @@ tl; dr
 curl -SL https://raw.githubusercontent.com/danpawlik/dotfiles/master/setup-vim.sh | bash
 ```
 
-## 1. Preparation:
-```
-mkdir -p ~/.vim/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/danpawlik/dotfiles.git /tmp/dotfiles
-```
+If you want to install some other vim distribution, that works well on `neovim` like:
 
-Install additional packages:
-```
-sudo apt install -y python-pip python3-pip || sudo yum install -y python-pip python3-pip
-pip install --user mypy
-pip3 install --user mypy
-```
+* [init.nvim](https://github.com/Blacksuan19/init.nvim)
+* [spacevim](https://github.com/SpaceVim/SpaceVim)
+* [Janus](https://github.com/carlhuda/janus)
+* [spf13](https://github.com/spf13/spf13-vim)
 
-If you want to use neovim:
+Just run:
 ```
-pip install --user neovim
-pip3 install --user neovim
-```
-
-## 2. Take one configuration:
-
-### - deoplete:
-
-Copy deoplete config file:
-```
-cp /tmp/dotfiles/vimrc/deoplete ~/.vimrc;
-```
-Install plugins:
-```
-vim +PlugInstall +qall
-```
-
-> If all is installed, check health by execuint command in vim/nvim:
-```
-:checkhealth
-```
-Thanks @Shougo/deoplete.nvim
-
-### - YouCompleteMe (YCM)
-
-```
-cp /tmp/dotfiles/vimrc/ycm ~/.vimrc
-```
-
-On the end, compile YCM for e.g. python:
-```
-sudo apt install -y build-essential cmake || sudo yum install -y automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel
-vim +PlugInstall +qall
-```
-Bellow commands are not required. Plugged will do that for us ;)
-```
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-```
-Thanks @Valloric/YouCompleteMe
-
-
-### - Jedi vim:
-```
-sudo apt install -y vim-python-jedi || sudo yum install -y vim-jedi python2-setuptools python3-setuptools
-cp /tmp/dotfiles/vimrc/jedi ~/.vimrc
-vim +PlugInstall +qall
-```
-Thanks @davidhalter/jedi-vim
-
-
-### - Spacevim
-
-Install using script:
-```
-curl -sLf https://spacevim.org/install.sh | bash
-```
-
-Copy configuration:
-```
-cp -a /tmp/dotfiles/.SpaceVim.d ~/.SpaceVim.d
-```
-
-Thats all!
-
-Thanks @SpaceVim/SpaceVim team!
-
-### - CoC vim:
-```
-curl -sL install-node.now.sh/lts | bash
-cp /tmp/dotfiles/vimrc/coc ~/.vimrc
-export LC_ALL=en_US.UTF-8
-vim +PlugInstall +qall
-for plugin in coc-sh coc-json coc-utils coc-pyright coc-html coc-yaml coc-prettier coc-snippets coc-python coc-git coc-go coc-docker; do
-vim +'CocInstall "$plugin"' +qall
-done
-```
-Thanks @neoclide/coc.nvim
-
-### - Tabnine vim:
-```
-cp /tmp/dotfiles/vimrc/tabnine ~/.vimrc
-vim +PlugInstall +qall
-```
-Thanks @codota/TabNine
-
-
-## 3. Neovim - configure and install:
-The best option if you don't have `vim > 8.0` is to
-replace vim with neovim.
-
-How to install for neovim and replace vim:
-
-1. Download latest nvim appimage:
-```
-curl -SL https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > /tmp/nvim.appimage
-sudo mv /tmp/nvim.appimage /usr/bin/nvim
-chmod u+x /usr/bin/nvim
-if [ -f "$HOME/.zshrc" ]; then
-  echo "alias vim=nvim" >> $HOME/.zshrc
-elif [ -f "$HOME/.bashrc" ]; then
-  echo "alias vim=nvim" >> $HOME/.bashrc
-fi
-```
-
-2. Create symlink neovim to vim:
-```
-mkdir -p $HOME/.config/nvim
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
-```
-
-3. Install plugged for neovim:
-```
-curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +qall
+curl -SL https://raw.githubusercontent.com/danpawlik/dotfiles/master/setup-vim.sh | CONFIGURE_VIM=false bash
 ```
 
 # ZSH and others
--- setup-vim.sh
+++ setup-vim.sh
@@ -1,73 +1,75 @@
 #!/bin/bash
 
+CONFIGURE_VIM=${CONFIGURE_VIM:-true}
 VIM_CONFIG="${VIM_CONFIG:-ycm}"
 SETUP_NEOVIM="${SETUP_NEOVIM:-true}"
 SOURCE_DIR=${SOURCE_DIR:-'/tmp/dotfiles'}
 
+if [ "$CONFIGURE_VIM" = "true" ]; then
+    mkdir -p ~/.vim/
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.vim/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-if ! [ -d "${SOURCE_DIR}" ]; then
-    git clone https://github.com/danpawlik/dotfiles.git "${SOURCE_DIR}"
-fi
-
-sudo apt install -y python-pip python3-pip || sudo yum install -y python3-pip
-pip3 install --user neovim mypy pynvim
-
-OS_VERSION=$(awk '{print $4}' /etc/centos-release | cut -f1 -d'.')
-
-cp -v "${SOURCE_DIR}/vimrc/common" "${HOME}/.vimrc"
-# FIXME: vimplug can not read from multiple paths, so at first install
-# common libraries then specified.
-vim +'PlugInstall --sync' +qall &> /dev/null < /dev/tty
-
-
-if [ "${VIM_CONFIG}" == 'ycm' ]; then
-    sudo apt install -y build-essential cmake || sudo yum install -y automake gcc gcc-c++ kernel-devel cmake python3-devel
-fi
-
-if [ $OS_VERSION -eq 7 ] && [ "${VIM_CONFIG}" == 'ycm' ]; then
-    yum install -y devtoolset-8 centos-release-scl cmake
-fi
-
-if [ "${VIM_CONFIG}" == 'jedi' ]; then
-    sudo apt install -y vim-python-jedi || sudo yum install -y vim-jedi python3-setuptools
-fi
-
-if [ "${VIM_CONFIG}" == 'coc' ]; then
-    export LC_ALL=en_US.UTF-8
-    curl -sL install-node.now.sh/lts | bash
-    vim +PlugInstall +qall
-    for plugin in coc-sh coc-json coc-utils coc-pyright coc-html \
-                  coc-yaml coc-prettier coc-snippets coc-python \
-                  coc-git coc-go coc-docker; do
-        vim +'CocInstall "$plugin"' +qall &> /dev/null < /dev/tty
-    done
-fi
-
-if [ "${VIM_CONFIG}" == 'spacevim' ]; then
-    curl -sLf https://spacevim.org/install.sh | bash
-    cp -av "${SOURCE_DIR}/.SpaceVim.d" ~/.SpaceVim.d
-fi
-
-if [ "${VIM_CONFIG}" != 'spacevim' ]; then
-    if [ -f ~/.vimrc ]; then
-        mv ~/.vimrc ~/.vimrc-common
+    if ! [ -d "${SOURCE_DIR}" ]; then
+        git clone https://github.com/danpawlik/dotfiles.git "${SOURCE_DIR}"
     fi
-    cp -v "${SOURCE_DIR}/vimrc/${VIM_CONFIG}" ~/.vimrc
-fi
 
-vim +'PlugInstall --sync' +qall &> /dev/null < /dev/tty
+    sudo apt install -y python-pip python3-pip || sudo yum install -y python3-pip
+    pip3 install --user neovim mypy pynvim
 
-if [ "${VIM_CONFIG}" == 'ycm' ]; then
-scl enable devtoolset-8 - << \EOF
+    OS_VERSION=$(awk '{print $4}' /etc/centos-release | cut -f1 -d'.')
+
+    cp -v "${SOURCE_DIR}/vimrc/common" "${HOME}/.vimrc"
+    # FIXME: vimplug can not read from multiple paths, so at first install
+    # common libraries then specified.
+    vim +'PlugInstall --sync' +qall &> /dev/null < /dev/tty
+
+
+    if [ "${VIM_CONFIG}" == 'ycm' ]; then
+        sudo apt install -y build-essential cmake || sudo yum install -y automake gcc gcc-c++ kernel-devel cmake python3-devel
+    fi
+
+    if [ $OS_VERSION -eq 7 ] && [ "${VIM_CONFIG}" == 'ycm' ]; then
+        yum install -y devtoolset-8 centos-release-scl cmake
+    fi
+
+    if [ "${VIM_CONFIG}" == 'jedi' ]; then
+        sudo apt install -y vim-python-jedi || sudo yum install -y vim-jedi python3-setuptools
+    fi
+
+    if [ "${VIM_CONFIG}" == 'coc' ]; then
+        export LC_ALL=en_US.UTF-8
+        curl -sL install-node.now.sh/lts | bash
+        vim +PlugInstall +qall
+        for plugin in coc-sh coc-json coc-utils coc-pyright coc-html \
+                      coc-yaml coc-prettier coc-snippets coc-python \
+                      coc-git coc-go coc-docker; do
+            vim +'CocInstall "$plugin"' +qall &> /dev/null < /dev/tty
+        done
+    fi
+
+    if [ "${VIM_CONFIG}" == 'spacevim' ]; then
+        curl -sLf https://spacevim.org/install.sh | bash
+        cp -av "${SOURCE_DIR}/.SpaceVim.d" ~/.SpaceVim.d
+    fi
+
+    if [ "${VIM_CONFIG}" != 'spacevim' ]; then
+        if [ -f ~/.vimrc ]; then
+            mv ~/.vimrc ~/.vimrc-common
+        fi
+        cp -v "${SOURCE_DIR}/vimrc/${VIM_CONFIG}" ~/.vimrc
+    fi
+
+    vim +'PlugInstall --sync' +qall &> /dev/null < /dev/tty
+
+    if [ "${VIM_CONFIG}" == 'ycm' ]; then
+    scl enable devtoolset-8 - << \EOF
 $HOME/.vim/plugged/YouCompleteMe/install.py
 EOF
+    fi
 fi
 
-if [ "${SETUP_NEOVIM}" = 'true' ]; then
+if [ "${SETUP_NEOVIM}" = "true" ]; then
     curl -SL https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > "${SOURCE_DIR}/nvim.appimage"
 
     if [ -f "/etc/centos-release" ] && cat /etc/os-release  | grep VERSION_ID | grep -q '7'; then
