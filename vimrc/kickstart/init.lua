66efd8cc55051adb7a07aedcee7fe3150fa58b2e
Daniel Pawlik <dpawlik@redhat.com>
2026-01-15 17:41:05 +0100

    Update neo-vim kickstart

--- vimrc/kickstart/init.lua
+++ vimrc/kickstart/init.lua
@@ -1,57 +1,221 @@
 -- from https://github.com/nvim-lua/kickstart.nvim
-vim.g.encoding = "uft-8"
--- vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.autoindent = true
--- vim.opt.background = "dark"
-vim.opt.hlsearch = true
-vim.g.nofoldenable = true
--- vim.g.wildmenu = true
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
-vim.opt.smartindent = true
-vim.opt.smartcase = true
-vim.g.nowrap = true
--- vim.g.ttyfast = true
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
--- vim.g.colorcolumn = 80, 120
-vim.opt.ignorecase = true
-vim.opt.signcolumn = "yes"
-vim.o.termguicolors = true
-vim.o.autoread = true
+--[[
+
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+
+What is Kickstart?
+
+  Kickstart.nvim is *not* a distribution.
+
+  Kickstart.nvim is a starting point for your own configuration.
+    The goal is that you can read every line of code, top-to-bottom, understand
+    what your configuration is doing, and modify it to suit your needs.
+
+    Once you've done that, you can start exploring, configuring and tinkering to
+    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+    or immediately breaking it into modular pieces. It's up to you!
+
+    If you don't know anything about Lua, I recommend taking some time to read through
+    a guide. One possible example which will only take 10-15 minutes:
+      - https://learnxinyminutes.com/docs/lua/
+
+    After understanding a bit more about Lua, you can use `:help lua-guide` as a
+    reference for how Neovim integrates Lua.
+    - :help lua-guide
+    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+
+Kickstart Guide:
+
+  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+
+    If you don't know what this means, type the following:
+      - <escape key>
+      - :
+      - Tutor
+      - <enter key>
+
+    (If you already know the Neovim basics, you can skip this step.)
+
+  Once you've completed that, you can continue working through **AND READING** the rest
+  of the kickstart init.lua.
+
+  Next, run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
+
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
+
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+  I have left several `:help X` comments throughout the init.lua
+    These are hints about where to find more information about the relevant settings,
+    plugins or Neovim features used in Kickstart.
+
+   NOTE: Look for lines like this
+
+    Throughout the file. These are for you, the reader, to help you understand what is happening.
+    Feel free to delete them once you know what you're doing, but they should serve as a guide
+    for when you are first encountering a few different constructs in your Neovim config.
+
+If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+
+I hope you enjoy your Neovim journey,
+- TJ
+
+P.S. You can delete this when you're done too. It's your config now! :)
+--]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.mouse = ""
-vim.opt.showmode = false
-vim.opt.clipboard = "unnamedplus"
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
 
--- vim.opt.inccommand = "split"
--- vim.opt.scrolloff = 10
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.o.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.o.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = ""
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- NEW
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- END NEW
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+-- vim.o.scrolloff = 10
+
+-- NEW
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+-- END NEW
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- dpawlik - deprecated
--- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
--- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
--- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+-- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -60,6 +224,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -69,16 +235,23 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 
+---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+
 require("lazy").setup({
-
-	-- remove white characters
-	{ "thirtythreeforty/lessspace.vim" },
-
-	-- end custom plugins
-
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
@@ -296,7 +469,18 @@ require("lazy").setup({
 	},
 
 	-- LSP Plugins
-	{ "folke/lazydev.nvim", config = true },
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 	{
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
@@ -762,27 +946,27 @@ require("lazy").setup({
 		},
 	},
 
-	--	{ -- You can easily change to a different colorscheme.
-	--		-- Change the name of the colorscheme plugin below, and then
-	--		-- change the command in the config to whatever the name of that colorscheme is.
-	--		--
-	--		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	--		"folke/tokyonight.nvim",
-	--		priority = 1000, -- Make sure to load this before all the other start plugins.
-	--		config = function()
-	--			---@diagnostic disable-next-line: missing-fields
-	--			require("tokyonight").setup({
-	--				styles = {
-	--					comments = { italic = false }, -- Disable italics in comments
-	--				},
-	--			})
-	--
-	--			-- Load the colorscheme here.
-	--			-- Like many other themes, this one has different styles, and you could load
-	--			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-	--			vim.cmd.colorscheme("tokyonight-night")
-	--		end,
-	--	},
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("tokyonight").setup({
+				styles = {
+					comments = { italic = false }, -- Disable italics in comments
+				},
+			})
+
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
 
 	-- Highlight todo, notes, etc in comments
 	{
@@ -832,68 +1016,68 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"cmake",
-				"comment",
-				"css",
-				"dhall",
-				"diff",
-				"dockerfile",
-				"gitcommit",
-				"git_rebase",
-				"go",
-				"graphql",
-				"haskell",
-				"helm",
-				"html",
-				"http",
-				"javascript",
-				"jq",
-				"json",
-				"lua",
-				"luadoc",
-				"make",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"rst",
-				"rust",
-				"toml",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-			incremental_selection = { enable = true },
-			textobjects = { enable = true },
-		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-	},
+	--	{ -- Highlight, edit, and navigate code
+	--		"nvim-treesitter/nvim-treesitter",
+	--		build = ":TSUpdate",
+	--		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+	--		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+	--		opts = {
+	--			ensure_installed = {
+	--				"bash",
+	--				"c",
+	--				"cmake",
+	--				"comment",
+	--				"css",
+	--				"dhall",
+	--				"diff",
+	--				"dockerfile",
+	--				"gitcommit",
+	--				"git_rebase",
+	--				"go",
+	--				"graphql",
+	--				"haskell",
+	--				"helm",
+	--				"html",
+	--				"http",
+	--				"javascript",
+	--				"jq",
+	--				"json",
+	--				"lua",
+	--				"luadoc",
+	--				"make",
+	--				"markdown",
+	--				"markdown_inline",
+	--				"python",
+	--				"query",
+	--				"regex",
+	--				"rst",
+	--				"rust",
+	--				"toml",
+	--				"typescript",
+	--				"vim",
+	--				"vimdoc",
+	--				"yaml",
+	--			},
+	--			-- Autoinstall languages that are not installed
+	--			auto_install = true,
+	--			highlight = {
+	--				enable = true,
+	--				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+	--				--  If you are experiencing weird indenting issues, add the language to
+	--				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+	--				additional_vim_regex_highlighting = { "ruby" },
+	--			},
+	--			indent = { enable = true, disable = { "ruby" } },
+	--			incremental_selection = { enable = true },
+	--			textobjects = { enable = true },
+	--		},
+	--		-- There are additional nvim-treesitter modules that you can use to interact
+	--		-- with nvim-treesitter. You should go explore a few and see what interests you:
+	--		--
+	--		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+	--		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+	--		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+	--	},
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -961,6 +1145,29 @@ require("lazy").setup({
 	},
 })
 
+-- My config --
+
+vim.g.encoding = "uft-8"
+-- vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+-- converts tabs into spaces
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+-- vim.opt.background = "dark"
+vim.opt.hlsearch = true
+vim.g.nofoldenable = true
+-- vim.g.wildmenu = true
+vim.opt.cursorcolumn = true
+vim.opt.smartindent = true
+vim.g.nowrap = true
+-- vim.g.ttyfast = true
+-- vim.g.colorcolumn = 80, 120
+vim.o.termguicolors = true
+vim.o.autoread = true
+
+-- Colorscheme
 -- vim.cmd([[silent! colorscheme PaperColor]])
 -- vim.cmd([[silent! colorscheme material]])
 -- vim.cmd([[silent! colorscheme tender]])
--- vimrc/kickstart/lua/kickstart/plugins/indent_line.lua
+++ vimrc/kickstart/lua/kickstart/plugins/indent_line.lua
@@ -1,18 +1,21 @@
+local highlight = {
+	"CursorColumn",
+	"Whitespace",
+}
+
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	config = function()
-		local highlight = {
-			"CursorColumn",
-			"Whitespace",
-		}
-		require("ibl").setup({
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {
 			indent = { highlight = highlight, char = "" },
 			whitespace = {
 				highlight = highlight,
 				remove_blankline_trail = false,
 			},
 			scope = { enabled = false },
-		})
-	end,
+		},
+	},
 }
