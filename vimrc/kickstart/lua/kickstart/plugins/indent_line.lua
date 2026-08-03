1ed940bccc2ccbf2f168b65cb88c9e00be07bdcd
Daniel Pawlik <dpawlik@redhat.com>
2026-08-03 13:43:18 +0200

    Migrate from lazy.nvim to vim.pack
    Signed-off-by: Daniel Pawlik <dpawlik@redhat.com>

--- vimrc/kickstart/lua/kickstart/plugins/indent_line.lua
+++ vimrc/kickstart/lua/kickstart/plugins/indent_line.lua
@@ -1,21 +1,7 @@
-local highlight = {
-	"CursorColumn",
-	"Whitespace",
-}
+-- Alternating colored indent guides (background only, no characters)
 
-return {
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = "ibl",
-		opts = {
-			indent = { highlight = highlight, char = "" },
-			whitespace = {
-				highlight = highlight,
-				remove_blankline_trail = false,
-			},
-			scope = { enabled = false },
-		},
-	},
-}
+vim.pack.add { 'https://github.com/preservim/vim-indent-guides' }
+vim.g.indent_guides_enable_on_vim_startup = 1
+vim.g.indent_guides_auto_colors = 0
+vim.cmd [[autocmd VimEnter,Colorscheme * highlight IndentGuidesOdd  guibg=#232526 ctermbg=235]]
+vim.cmd [[autocmd VimEnter,Colorscheme * highlight IndentGuidesEven guibg=#293739 ctermbg=236]]
